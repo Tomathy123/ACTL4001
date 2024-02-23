f9e3614d47b4a1730511622087797978ed844361
arneetkalra2 <107466528+arneetkalra2@users.noreply.github.com>
2024-02-23 19:16:39 +1100

    Add files via upload

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -1,12 +1,13 @@
-
 # Setting Up --------------------------------------------------------------
 cat("\014") # Clear console
 rm(list=ls()) # Clear Environment
-setwd("/Users/arneetkalra/Desktop/UNSW Onedrive/UNI/2024/ACTL4001/Assignment/Data") #change to your own
+
+setwd("/Users/arneetkalra/Desktop/UNSW Onedrive/UNI/2024/ACTL4001/Assignment/Data")
 
 library(tidyverse)
 library(dplyr)
 library(readxl)
+library(openxlsx)
 
 inforce_data <- read.csv("2024-srcsc-superlife-inforce-dataset.csv", header = TRUE)
 intervention_data <- read_excel("srcsc-2024-interventions.xlsx")
@@ -41,5 +42,36 @@ mortality_data <- mortality_data[-c(1:7),]
 # Remove columns 3-8
 mortality_data <- mortality_data[,-c(3:8)]
 
+### Converting all characters to numbers in Economics Data
+eco_data$Year <- as.numeric(eco_data$Year)
+eco_data$Inflation <- as.numeric(eco_data$Inflation)
+eco_data$`Government of Lumaria Overnight Rate` <- as.numeric(eco_data$`Government of Lumaria Overnight Rate`)
+eco_data$`1-yr Risk Free Annual Spot Rate` <- as.numeric(eco_data$`1-yr Risk Free Annual Spot Rate`)
+eco_data$`10-yr Risk Free Annual Spot Rate` <- as.numeric(eco_data$`10-yr Risk Free Annual Spot Rate`)
+
+### Converting all characters to numbers in Inforce Data
+inforce_data$Issue.year <- as.numeric(inforce_data$Issue.year)
+inforce_data$Issue.age <- as.numeric(inforce_data$Issue.age)
+inforce_data$Face.amount <- as.numeric(inforce_data$Face.amount)
+inforce_data$Region <- as.numeric(inforce_data$Region)
+inforce_data$Death.indicator <- as.numeric(inforce_data$Death.indicator)
+inforce_data$Year.of.Death <- as.numeric(inforce_data$Year.of.Death)
+inforce_data$Lapse.Indicator <- as.numeric(inforce_data$Lapse.Indicator)
+inforce_data$Year.of.Lapse <- as.numeric(inforce_data$Year.of.Lapse)
+
+### Converting all characters to numbers in Mortality Data
+mortality_data$Age <- as.numeric(mortality_data$Age)
+mortality_data$`Mortality Rate` <- as.numeric(mortality_data$`Mortality Rate`)
+
+### Exporting in an excel file
+write.xlsx(eco_data, "economy_data.xlsx")
+write.xlsx(inforce_data, "inforce_data.xlsx")
+write.xlsx(mortality_data, "mortality_data.xlsx")
+write.xlsx(intervention_data, "intervention_data.xlsx")
+
+write_csv(intervention_data, "intervention_data.csv")
+write_csv(eco_data, "economy_data.csv")
+write_csv(inforce_data, "inforce_data.csv")
+write_csv(mortality_data, "mortality_data.csv")
 
 
