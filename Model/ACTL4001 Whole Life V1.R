84112f0a9f890eb5232656a4fae6dee23d14d136
arneetkalra2 <107466528+arneetkalra2@users.noreply.github.com>
2024-03-16 17:38:02 +1100

    Update ACTL4001 Whole Life V1.R

--- Model/ACTL4001 Whole Life V1.R
+++ Model/ACTL4001 Whole Life V1.R
@@ -1,10 +1,14 @@
+# Setting Up --------------------------------------------------------------
+cat("\014") # Clear console
 rm(list=ls()) # Clear Environment
 
+
+# Directory ---------------------------------------------------------------
 setwd("/Users/arneetkalra/Desktop/UNSW Onedrive/UNI/2024/ACTL4001/Assignment/Data") #change to your own
 # setwd("C:/Users/aluis/Documents/UNSW/ACTL4001") 
-# setwd("C:/Users/Anoushay/Desktop/ACTL4001/Cleaned Data")
-# setwd("/Users/karandeshwal/Documents/R/ACTL4001")
 
+
+# Packages ----------------------------------------------------------------
 # install.packages('tidyverse')
 # install.packages('dplyr')
 # install.packages('readxl')
@@ -17,14 +21,14 @@ library(tidyr)
 library(openxlsx)
 
 
-# Read in the Data (files in the drive)
+# Read in the Data (files in the drive) -----------------------------------
 inforce_data <- read.csv("inforce_data.csv", header = TRUE)
 intervention_data <- read.csv("intervention_data.csv", header = TRUE)
 eco_data <- read.csv("economy_data.csv", header = TRUE)
 mortality_data <- read.csv("mortality_data.csv", header = TRUE)
 
 
-# Base Mortality Table Calculation
+# Base Mortality Table Calculation ----------------------------------------
 #Removing the additional column
 mortality_data <- mortality_data[,-c(1)]
 #finding the probability of survival at each age
@@ -35,104 +39,41 @@ average_spot_rate <- mean(eco_data[,4])
 #This is used, if current year is past 2023
 
 
-insurance_20_year <- function (x, issue_year) {
-  kpx <- rep(0,20)
+
+# Whole Life Function -----------------------------------------------------
+whole_life <- function(x, issue_year) {
+  max_age <- max(which(!is.na(mortality_data$p_x)))
+  n_years <- max_age - x
   
-  kpx[1] <- mortality_data$p_x[x]
-  
-  for (i in 2:20) {
-    kpx[i] <- prod(kpx[i-1],mortality_data$p_x[x+i-1])
+  # Calculate survival probabilities (kpx)
+  kpx <- numeric(n_years + 1)
+  kpx[1] <- 1 # Initial survival probability is 1
+  for (i in 2:length(kpx)) {
+    kpx[i] <- kpx[i-1] * mortality_data$p_x[x + i - 2] # Corrected to use actual survival rates
   }
   
-  #Find kpx values, will use formula k|q_x=kpx*q_(x+k)
-  
-  spot_rate_x <- rep(0,20)
-  
-  #This checks if current year is less than 2023, if so, use spot rate, otherwise use average
-  for (i in 1:20) {
+  # Calculate spot rates for each year
+  spot_rate_x <- rep(average_spot_rate, n_years) # Default to average if no data available
+  for (i in 1:n_years) {
     current_year <- issue_year + i - 1
-    if (length(eco_data$X1.yr.Risk.Free.Annual.Spot.Rate[eco_data$Year == current_year]) == 0) {
-      spot_rate_x[i] <- average_spot_rate
-    } else {
-      spot_rate_x[i] <- eco_data$X1.yr.Risk.Free.Annual.Spot.Rate[eco_data$Year == current_year]
+    if (current_year <= 2023) { # Assuming you have spot rate data up to 2023
+      spot_rate_x[i] <- ifelse(is.na(eco_data$X1.yr.Risk.Free.Annual.Spot.Rate[eco_data$Year == current_year]), average_spot_rate, eco_data$X1.yr.Risk.Free.Annual.Spot.Rate[eco_data$Year == current_year])
     }
   }
   
-  #This should generate a vector of (1+i,(1+i)^2,...,(1+i)^20)
-  effective_interest <- c(1+spot_rate_x[1])
-  for (i in 2:20) {
-    effective_interest[i] <- prod(effective_interest[i-1], (1 + spot_rate_x[i]))
+  # Calculate present value factors (discount factors)
+  v <- 1 / (1 + spot_rate_x) # Direct calculation of discount factors
+  
+  # Calculate the values (present value of expected benefits)
+  value <- numeric(n_years)
+  for(i in 1:n_years) {
+    value[i] <- kpx[i] * mortality_data$Mortality.Rate[x+i-1] * prod(v[1:i])
   }
   
-  #Finding the components of the sum for the final value
-  # Using sum of v^(k+1)*kpx*q_(x+k)
-  value <- rep(0,20)
-  value[1] <- prod(mortality_data$Mortality.Rate[x], 
-                   (1/(effective_interest[1])))
-  
-  for (i in 1:19) {
-    value[i+1] <- prod(kpx[i],mortality_data$Mortality.Rate[x+i], 1/(effective_interest[i+1]))
-  }
-  
-  final <- sum(value)
-  return(final)
-  
-}
-
-###IMPORTANT
-#I think there is a small error somewhere as it is returning values which don't seem right
-#When compared to AM92 in orange book.
-
-insurance_20_year(40, 2000)
-
-
-#Annuity
-
-annuity_due_term <- function (x, issue_year) {
-  
-  kpx <- rep(0,20)
-  
-  kpx[1] <- mortality_data$p_x[x]
-  
-  for (i in 2:20) {
-    kpx[i] <- prod(kpx[i-1],mortality_data$p_x[x+i-1])
-  }
-  
-  
-  spot_rate_x <- rep(0,19)
-  
-  #This checks if current year is less than 2023, if so, use spot rate, otherwise use average
-  for (i in 1:19) {
-    current_year <- issue_year + i - 1
-    if (length(eco_data$X1.yr.Risk.Free.Annual.Spot.Rate[eco_data$Year == current_year]) == 0) {
-      spot_rate_x[i] <- average_spot_rate
-    } else {
-      spot_rate_x[i] <- eco_data$X1.yr.Risk.Free.Annual.Spot.Rate[eco_data$Year == current_year]
-    }
-  }
-  
-  #This should generate a vector of (1+i,(1+i)^2,...,(1+i)^20)
-  effective_interest <- c(1+spot_rate_x[1])
-  for (i in 2:19) {
-    effective_interest[i] <- prod(effective_interest[i-1], (1 + spot_rate_x[i]))
-  }
-  
-  
-  value <- rep(0,20)
-  value[1] <- 1
-  
-  for (i in 1:19) {
-    value[i+1] <- prod(kpx[i],1/effective_interest[i])
-  }
-  
-  
-  
-  final <- sum(value)
-  return(final)
-  
+  final_value <- sum(value)
+  return(final_value)
 }
 
 
-50000*insurance_20_year(40,2000)
+50000*whole_life_corrected(40,2023)
 
-2262.102/annuity_due_term(40,2000)
